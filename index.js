4ee104447815def8408a9093f310c9c1fa4fb298
daadestroyer <nigamshubham2000@gmail.com>
2021-01-25 23:44:44 +0530

    JS Variables and Datatypes completed...

---
+++ index.js
@@ -0,0 +1,38 @@
+// 1. Javascript Variables
+var num1 = 10;
+var num2 = 20;
+console.log(num1+num2)
+
+// 2. Datatypes in Javascsript 
+/*
+At a very high level there are 2 types of datatypes in JS
+1. Primitive Data Types: string , number , boolean , undefinded , symbol
+2. Reference Data Types: 
+*/
+
+// String
+var str1 = "this is string"
+var str2 = "this is also a string"
+
+// Numbers
+var num1 = 455
+var num2 = 4.55
+console.log(str1+" : "+str2+" "+num1+num2)
+
+// Objects
+var marks = {
+    shubham : 34,
+    shubhanshu : 33,
+    sakshi : 40
+}   
+console.log(marks)
+
+// Booleans
+var a = true
+var b = false
+var c
+console.log(a+":"+b+":"+c)
+
+//  Null
+var n = null
+console.log(n)
